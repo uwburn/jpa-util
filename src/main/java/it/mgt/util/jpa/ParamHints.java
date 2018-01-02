cfb36292fb16be623121a8340c9a4f6a5d143b94
Michele Tibaldi <michele.tibaldi@holonix.it>
2018-01-02 19:36:46 +0100

    Initial commit.

---
+++ src/main/java/it/mgt/util/jpa/ParamHints.java
@@ -0,0 +1,15 @@
+package it.mgt.util.jpa;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(TYPE)
+public @interface ParamHints {
+
+	ParamHint[] value();
+	
+}
